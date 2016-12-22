db5290115e650c92aa2e3ed52c1b25178fd2c916
Pablo Ruiz-Muzquiz <diacritica@gmail.com>
2016-12-23 00:05:58 +0100

    added ALEARNER solver

---
+++ back/api/solvers/alearner.py
@@ -0,0 +1,103 @@
+import itertools
+from pymongo import MongoClient
+from random import randint
+
+#SETTINGS
+connection = MongoClient('localhost', 27017)
+db = connection.testdb
+
+class Solver:
+
+    '''
+    Must implement
+    __init__(self, rtkid)
+    newpair(self)
+    pickchoice(self, winner, loser)
+    getprogress(self)
+    getorderedlist(self)
+    save(self)
+    '''
+
+    def __init__(self, rtkid):
+
+        self.rtkid = rtkid
+        self.rtk = db['rtks'].find_one({'id':rtkid})
+        self.custom = {}
+
+        self.list = self.rtk['elements']
+        self.orderedlist = self.rtk['orderedlist']
+        self.orderprogress = self.rtk["orderprogress"]
+
+        try:
+            self.custom["choicesmade"] = self.rtk['custom']['choicesmade']
+        except:
+            self.custom["choicesmade"] = {n:[] for n in self.list}
+
+        if self.orderprogress > 0.0:
+            self.candidatepairs = self.rtk["candidatepairs"]
+            self.origcandidatepairs = self.rtk["origcandidatepairs"]
+        else:
+            self.generatecandidatepairs()
+
+    def __repr__(self):
+        return str(self.orderedlist)
+
+    def generatecandidatepairs(self):
+
+        tuplelistofcandidates = list(itertools.combinations(self.list,2))
+        candidates = [[n[0],n[1]] for n in tuplelistofcandidates]
+        self.candidatepairs = candidates
+        self.origcandidatepairs = float(len(candidates))
+
+
+    def removecandidatepair(self,pair):
+
+        try:
+            self.candidatepairs.remove([pair[0],pair[1]])
+        except:
+            self.candidatepairs.remove([pair[1],pair[0]])
+
+
+    def newpair(self):
+        try:
+            randompos = randint(0,len(self.candidatepairs)-1)
+            pair = self.candidatepairs[randompos]
+            return pair
+        except:
+            return (-1,-1)
+
+    def pickchoice(self, winner, loser):
+
+        if winner == -1:
+            return 1.0
+
+        self.custom["choicesmade"][winner].append(loser)
+
+        #review any inferred knowledge
+        for n in self.custom["choicesmade"].items():
+            if n[0] is not winner:
+                if loser not in n[1]:
+                    if winner in n[1]:
+                        n[1].append(loser)
+                        self.removecandidatepair((n[0],loser))
+
+
+        import operator
+        self.removecandidatepair([winner,loser])
+        self.orderedlist = [(w[0],len(w[1])) for w in self.custom["choicesmade"].items()]
+        self.orderedlist.sort(key=operator.itemgetter(1), reverse=True)
+
+        self.orderprogress = (self.origcandidatepairs-len(self.candidatepairs))/self.origcandidatepairs
+        return self.orderprogress
+
+    def getprogress(self):
+        return self.orderprogress
+
+    def getorderedlist(self):
+        return self.orderedlist
+
+    def save(self):
+        pythonobject = {"candidatepairs":self.candidatepairs,"origcandidatepairs":self.origcandidatepairs,
+        "orderedlist":self.orderedlist,"orderprogress":self.orderprogress,"custom":{"choicesmade":self.custom["choicesmade"]}}
+        db['rtks'].update({"id":self.rtkid}, {"$set": pythonobject},upsert=False)
+
